20d0133cf00665b17436978499e63aae753ebff4
Arturo Milanes <arturo.e.m.n@gmail.com>
2024-02-19 23:05:52 -0800

    added random button

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
 document.querySelector("#button").addEventListener('click', () => {
-  document.querySelector("#content").style.color = 'red';
+  document.querySelector("#content").style.color = `#${randomColor}`;
 })
