028e9f7052bb02a6d24e4670359971a604703abb
Arturo Milanes <arturo.e.m.n@gmail.com>
2024-02-20 18:30:35 -0800

    added rollbar

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8a1eeecec9e143949f5a9151b6480e0d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 /*
 __dirname: This is sort of like inserting the result of running pwd in your command line – it finds
 the full path of your server file. This is useful for running your server in multiple
